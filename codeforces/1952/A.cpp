e1ce7ac46f2ee19060a2b7b06d83f6c4151793d5
Sadman Islam <inboxsadman@gmail.com>
2024-04-02 08:58:00 +0600

    Add solution for problem `A - Are You a Robot, Again?` Link: https://codeforces.com/contest/1952/problem/A Tags: strings Ref: https://codeforces.com/contest/1952/submission/254650444

---
+++ codeforces/1952/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string s = "safety";
+    cout << s;
+    
+    return 0;
+}
